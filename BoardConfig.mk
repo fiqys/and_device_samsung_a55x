3694e25b1b8d3e160506554a4a12646c60712d80
fiqys <elsifycritic@gmail.com>
2026-08-23 18:46:05 +0300

    a55x: Setup VINTF
    Change-Id: I9cfe53eca819b7a35c5638f7e595f57c903f1723
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,3 +7,7 @@ DEVICE_PATH := device/samsung/a55x
 
 # Security
 VENDOR_SECURITY_PATCH := 2026-05-05
+
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
