55663aa5536770a93d5dc247281cf1196c66a78a
fiqys <elsifycritic@gmail.com>
2026-08-23 18:30:22 +0300

    a55x: Setup proprietary-files
    Change-Id: If1708d782cc8f37f663103f3ed11f1cb277d7b01
    Signed-off-by: fiqys <elsifycritic@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# Copyright (C) The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/samsung/a55x',
+    'vendor/samsung/a55x',
+]
+
+blob_fixups: blob_fixups_user_type = {
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'a55x',
+    'samsung',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
