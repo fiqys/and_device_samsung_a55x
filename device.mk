6c6cfe424f5c6aec69aedfa8b5a11e1871005b95
fiqys <elsifycritic@gmail.com>
2026-08-23 18:26:54 +0300

    a55x: Initial skeleton
    Change-Id: I09664ab2ffa0b09e3d0f8459ec957e7ec35f23ed
    Signed-off-by: fiqys <elsifycritic@gmail.com>

---
+++ device.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/samsung/a55x
+
+# Soong Namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
---
+++ lineage_a55x.mk
@@ -0,0 +1,12 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from a55x device
+$(call inherit-product, device/samsung/a55x/device.mk)
+
+# Device identifier, this must come after all inclusions
+PRODUCT_DEVICE := a55x
+PRODUCT_NAME := lineage_a55x
+PRODUCT_MODEL := SM-A556B
