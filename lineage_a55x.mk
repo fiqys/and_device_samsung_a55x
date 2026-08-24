4013b455e74ffb126d71f68c54e7e1937deb0a26
fiqys <elsifycritic@gmail.com>
2026-08-24 11:47:56 +0300

    a55x: Define device descriptors
    Change-Id: I53dc520a953bb65f22b07df70c2e02146f264103
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- lineage_a55x.mk
+++ lineage_a55x.mk
@@ -10,3 +10,9 @@ $(call inherit-product, device/samsung/a55x/device.mk)
 PRODUCT_DEVICE := a55x
 PRODUCT_NAME := lineage_a55x
 PRODUCT_MODEL := SM-A556B
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="a55xnaxx-user 14 UP1A.231005.007 A556BXXSECZDE release-keys" \
+    BuildFingerprint=samsung/a55xnaxx/a55x:14/UP1A.231005.007/A556BXXSECZDE:user/release-keys \
+    DeviceProduct=a55xnaxx \
+    SystemName=a55xnaxx
