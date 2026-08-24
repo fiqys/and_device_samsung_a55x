ef739c560f9eee3424252bb208d9ae0513b00431
luk1337 <priv.luk@gmail.com>
2026-08-24 10:47:48 +0300

    a55x: Add A/B updates support
    Change-Id: I5c47cfed9a05459d928bd44608f98a805dbaecbf
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -5,6 +5,20 @@
 
 DEVICE_PATH := device/samsung/a55x
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_client \
+    update_engine_sideload \
+    update_verifier
+
 # Kernel Modules
 PRODUCT_PACKAGES += \
     toolbox.vendor_ramdisk
