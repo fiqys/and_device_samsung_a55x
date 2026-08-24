82b712087a36ed92022df230aab4fc09370945ea
Yifan Hong <elsk@google.com>
2026-08-24 11:11:32 +0300

    a55x: Enforce generic ramdisk allow list
    This change enforces that only the list of files on the generic
    ramdisk can be installed to the ramdisk of this device.
    
    This is required for all devices using generic ramdisk.
    
    Change-Id: I91730f6fabfd54f55ee8431ab7c390e81072fdd8
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/media_vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Inherit common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
